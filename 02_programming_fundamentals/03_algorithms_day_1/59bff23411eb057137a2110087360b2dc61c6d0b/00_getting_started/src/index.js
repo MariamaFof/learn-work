fada009c40cef9e280ad4e398c4d3e89146b84c5
Mariama <f.mariame35@gmail.com>
2020-09-11 12:16:55 +0200

    “work”

---
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/00_getting_started/src/index.js
@@ -0,0 +1,6 @@
+// This variable needs to be changed to `true`.
+const boooleanVariable = true;
+
+const nullVariable = null;
+// This varible needs to be entered the exact text "Hello World!".
+const greetingVariable = "Hello World!";
---
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/02_play_with_arrays/src/02_arrays_methods.js
@@ -0,0 +1,24 @@
+/*
+ ## Use some methods
+ */
+
+const digits = [];
+
+// Rewrite your array `digits` using `push`;
+digits.push(0);
+digits.push(1);
+digits.push(2);
+digits.push(3);
+digits.push(4);
+digits.push(5);
+digits.push(6); 
+digits.push(7);
+digits.push(8); 
+digits.push(9);
+
+// rewrite your variable `last` using `length`;
+
+
+// create another array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
+
+// use `join` to create, into the variable `allDigits`, a string like this : `zero - one ...`.
